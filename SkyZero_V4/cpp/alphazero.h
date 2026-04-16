0fcf4d1c7a2b66cb7cb2f0ba4dd868242bfd64cd
Sky <skycheng159357@gmail.com>
2026-04-16 16:22:08 +0800

    Do not train cheap search

--- SkyZero_V4/cpp/alphazero.h
+++ SkyZero_V4/cpp/alphazero.h
@@ -43,7 +43,7 @@ struct AlphaZeroConfig {
     int cheap_simulations = 64;
     int cheap_gumbel_m = 8;
     float full_search_prob = 0.25f;
-    float cheap_sample_weight = 0.1f;
+    float cheap_sample_weight = 0.0f;  // KataGomo-aligned: cheap rows drop entirely (cheapSearchTargetWeight=0)
 
     // Exploration temperature
     int half_life = -1;                  // Python: args.get("half_life", game.board_size)
