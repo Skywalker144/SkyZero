5645ad61e753fefae3ac2e3e7bd2dfc77d08f505
Sky <skycheng159357@gmail.com>
2026-04-25 18:10:18 +0800

    Remove move_temperature read

--- SkyZero_V4/cpp/mcts_probe_main.cpp
+++ SkyZero_V4/cpp/mcts_probe_main.cpp
@@ -115,8 +115,6 @@ int main(int argc, char** argv) {
         cfg.gumbel_c_visit = cfg_get<float>(cfg_map, "GUMBEL_C_VISIT", 50.0f);
         cfg.gumbel_c_scale = cfg_get<float>(cfg_map, "GUMBEL_C_SCALE", 1.0f);
         cfg.half_life = cfg_get<int>(cfg_map, "HALF_LIFE", -1);
-        cfg.move_temperature_init = cfg_get<float>(cfg_map, "MOVE_TEMPERATURE_INIT", 0.8f);
-        cfg.move_temperature_final = cfg_get<float>(cfg_map, "MOVE_TEMPERATURE_FINAL", 0.2f);
         cfg.c_puct = cfg_get<float>(cfg_map, "C_PUCT", 1.1f);
         cfg.c_puct_log = cfg_get<float>(cfg_map, "C_PUCT_LOG", 0.45f);
         cfg.c_puct_base = cfg_get<float>(cfg_map, "C_PUCT_BASE", 500.0f);
