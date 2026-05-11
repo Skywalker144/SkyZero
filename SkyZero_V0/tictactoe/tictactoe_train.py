4fdfe666ebadf2a31daebb276ca994544057a147
Skywalker144 <skycheng159357@gmail.com>
2026-05-11 21:43:11 +0800

    Optimize hyperparameters

--- SkyZero_V0/tictactoe/tictactoe_train.py
+++ SkyZero_V0/tictactoe/tictactoe_train.py
@@ -19,23 +19,23 @@ train_args = {
     "weight_decay": 3e-5,  # 权重衰减系数
 
     "num_simulations": 50,      # 模拟次数
-    "dirichlet_alpha": 0.3,     # 控制Dirichlet噪声的锐度，不用动
+    "dirichlet_alpha": 1,     # 控制Dirichlet噪声的锐度，不用动
     "dirichlet_epsilon": 0.25,  # 控制Dirichlet噪声在根节点的占比，不用动
 
     "batch_size": 128,                # 训练时的batch_size
     "train_steps_per_iteration": 50,  # train阶段训多少个batch
     "target_ReplayRatio": 4,          # 样本回放率，即每个样本会被训练的次数。
 
-    "temperature": 1.0,   # 落子温度
-    "temp_threshold": 6,  # 每一句经过这个步数之后改为argmax落子
+    "move_temperature": 1.0,   # 落子温度
+    "half_life": 6,            # 每一局经过这个步数之后改为argmax落子
 
-    "min_buffer_size": 500,         # 最小多少样本开始训
-    "max_buffer_size": 100000,      # 最大容纳多少样本（受内存限制）
+    "min_buffer_size": 6400,        # 最小多少样本开始训（需 >= batch_size * train_steps_per_iteration）
+    "max_buffer_size": 1e7,      # 最大容纳多少样本（受内存限制）
     "window_exponent": 0.65,        # 不用动
     "window_expand_per_row": 0.4,   # 不用动
     
     "save_interval": 10,           # 隔几个iteration保存一次checkpoint
-    "data_dir": "data/tictactoe",
+    "data_dir": "data/tictactoe_",
     "device": "mps",
 
     "num_workers": 12,     # 并行worker数量
